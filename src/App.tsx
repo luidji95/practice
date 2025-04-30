918fe2b0e5811bde1d44ad0a4b63afd7bdf28d41
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-04-30 22:09:26 +0200

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,50 @@
 import { useState } from "react";
 
-const studenti = [
-  { id: 1, ime: "Ana", ocena: 8 },
-  { id: 2, ime: "Marko", ocena: 5 },
-  { id: 3, ime: "Luka", ocena: 9 },
-  { id: 4, ime: "Mina", ocena: 6 },
-  { id: 5, ime: "Ivana", ocena: 4 },
-];
-
 function App() {
-  const [djaci, setDjaci] = useState(studenti);
-  const [prosecnaOcena, setProsecnaOcena] = useState(0);
+  const [listaKnjiga, setListaKnjiga] = useState([]);
+  const [imeKnjige, setImeKnjige] = useState("");
+  const [serachTerm, setSearchTerm] = useState("");
 
-  const noviDjaci = djaci.filter((caci) => caci.ocena >= 6);
+  const dodajKnjigu = () => {
+    if (imeKnjige.trim() === "") return;
 
-  const prosecnaOcenaPolozenih = () => {
-    setProsecnaOcena(
-      noviDjaci.reduce((acc, caci) => acc + caci.ocena, 0) / noviDjaci.length
-    );
+    const novaKnjiga = {
+      id: Date.now(),
+      title: imeKnjige,
+    };
+
+    setListaKnjiga([...listaKnjiga, novaKnjiga]);
+    setImeKnjige("");
   };
+
+  const knjigeZaPrikaz =
+    serachTerm.length > 3
+      ? listaKnjiga.filter((knjiga) =>
+          knjiga.title.toLowerCase().includes(serachTerm.toLowerCase())
+        )
+      : listaKnjiga;
+
   return (
     <div>
-      <h2>Hello</h2>
-      {djaci.map((caci) => (
-        <p key={caci.id}>
-          {caci.ime} - {caci.ocena}
-        </p>
+      <h1>Spisak knjiga</h1>
+      <input
+        type="text"
+        placeholder="Unesite ime knjige!"
+        value={imeKnjige}
+        onChange={(e) => setImeKnjige(e.target.value)}
+      />
+      <button onClick={() => dodajKnjigu()}>Dodaj knjigu</button>
+
+      <input
+        type="text"
+        placeholder="Pretrazi knjigu"
+        value={serachTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {knjigeZaPrikaz.map((knjiga) => (
+        <p key={knjiga.id}>{knjiga.title}</p>
       ))}
-      <button onClick={() => prosecnaOcenaPolozenih()}>Klik</button>
-      <p>Prosecna ocena studenata koji su polozili je : {prosecnaOcena}</p>
     </div>
   );
 }
