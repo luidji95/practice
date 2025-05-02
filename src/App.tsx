2fea2ca7577ea398763287fe91d1f1aae6813ac6
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-05-02 17:09:25 +0200

    pracite

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,141 @@
 import { useState } from "react";
 
 function App() {
-  const [listaKnjiga, setListaKnjiga] = useState([]);
-  const [imeKnjige, setImeKnjige] = useState("");
-  const [serachTerm, setSearchTerm] = useState("");
+  const [proizvodi, setProizvodi] = useState([]);
+  const [artikal, setArtikal] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectSort, setSelectSort] = useState("");
+  const [prikaziSamoKupljene, setPrikaziSamoKupljene] = useState(false);
+  const [categoryInput, setCategoryInput] = useState("Hrana");
+  const [filtriranaKategorija, setFiltriranaKategorija] = useState("all");
 
-  const dodajKnjigu = () => {
-    if (imeKnjige.trim() === "") return;
+  const dodajArtikal = () => {
+    if (artikal.trim() === "") return;
 
-    const novaKnjiga = {
+    const newArtikal = {
       id: Date.now(),
-      title: imeKnjige,
+      nazivArtikla: artikal,
+      cenaArtikla: Math.floor(Math.random() * 901) + 100,
+      kategorija: categoryInput,
+      kupljeno: false,
     };
 
-    setListaKnjiga([...listaKnjiga, novaKnjiga]);
-    setImeKnjige("");
+    setProizvodi([...proizvodi, newArtikal]);
+    setArtikal("");
   };
 
-  const knjigeZaPrikaz =
-    serachTerm.length > 3
-      ? listaKnjiga.filter((knjiga) =>
-          knjiga.title.toLowerCase().includes(serachTerm.toLowerCase())
-        )
-      : listaKnjiga;
+  const prikazaniArtikli = proizvodi
+    .filter((artkl) =>
+      artkl.nazivArtikla.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .filter((artkl) => (prikaziSamoKupljene ? artkl.kupljeno === true : true))
+    .filter((artkl) =>
+      filtriranaKategorija === "all"
+        ? true
+        : artkl.kategorija === filtriranaKategorija
+    );
+
+  const obrisiProizvod = (id) => {
+    const noviNiz = proizvodi.filter((pr) => pr.id !== id);
+    setProizvodi(noviNiz);
+  };
+
+  const toggleKupljeno = (id) => {
+    const noviNiz = proizvodi.map((proizvod) =>
+      proizvod.id === id
+        ? { ...proizvod, kupljeno: !proizvod.kupljeno }
+        : proizvod
+    );
+
+    setProizvodi(noviNiz);
+  };
+  const ukupnaCenaKupljenih = proizvodi
+    .filter((p) => p.kupljeno)
+    .reduce((zbir, p) => zbir + p.cenaArtikla, 0);
+
+  const handleSort = (sortValue) => {
+    let sortirani = [...proizvodi];
+
+    if (sortValue === "a-z") {
+      sortirani.sort((a, b) => a.nazivArtikla.localeCompare(b.nazivArtikla));
+    } else if (sortValue === "z-a") {
+      sortirani.sort((a, b) => b.nazivArtikla.localeCompare(a.nazivArtikla));
+    } else if (sortValue === "price-low") {
+      sortirani.sort((a, b) => a.cenaArtikla - b.cenaArtikla);
+    } else if (sortValue === "price-high") {
+      sortirani.sort((a, b) => b.cenaArtikla - a.cenaArtikla);
+    }
+
+    setProizvodi(sortirani);
+  };
 
   return (
     <div>
-      <h1>Spisak knjiga</h1>
+      <h1>Lista proizvoda</h1>
       <input
         type="text"
-        placeholder="Unesite ime knjige!"
-        value={imeKnjige}
-        onChange={(e) => setImeKnjige(e.target.value)}
+        placeholder="Unesite novi proizvod"
+        value={artikal}
+        onChange={(e) => setArtikal(e.target.value)}
       />
-      <button onClick={() => dodajKnjigu()}>Dodaj knjigu</button>
-
+      <select
+        value={categoryInput}
+        onChange={(e) => setCategoryInput(e.target.value)}
+      >
+        <option value="Hrana">Hrana</option>
+        <option value="Tehnika">Tehnika</option>
+        <option value="Kuhinja">Kuhinja</option>
+      </select>
+      <button onClick={dodajArtikal}>Dodaj artikal u korpu</button>
+      <hr />
       <input
         type="text"
-        placeholder="Pretrazi knjigu"
-        value={serachTerm}
+        placeholder="Pretrazi artikal"
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-
-      {knjigeZaPrikaz.map((knjiga) => (
-        <p key={knjiga.id}>{knjiga.title}</p>
+      <select
+        value={selectSort}
+        onChange={(e) => {
+          setSelectSort(e.target.value);
+          handleSort(e.target.value);
+        }}
+      >
+        <option value="all">All</option>
+        <option value="a-z">Naziv A–Z</option>
+        <option value="z-a">Naziv Z–A</option>
+        <option value="price-low">Cena rastuće</option>
+        <option value="price-high">Cena opadajuće</option>
+      </select>
+      <input
+        type="checkbox"
+        checked={prikaziSamoKupljene}
+        onChange={() => setPrikaziSamoKupljene((prev) => !prev)}
+      />{" "}
+      Prikaži samo kupljene
+      <select
+        value={filtriranaKategorija}
+        onChange={(e) => setFiltriranaKategorija(e.target.value)}
+      >
+        <option value="all">Sve kategorije</option>
+        <option value="Hrana">Hrana</option>
+        <option value="Kuhinja">Kuhinja</option>
+        <option value="Tehnika">Tehnika</option>
+      </select>
+      <hr />
+      {prikazaniArtikli.map((proizvod) => (
+        <p key={proizvod.id}>
+          <input
+            type="checkbox"
+            checked={proizvod.kupljeno}
+            onChange={() => toggleKupljeno(proizvod.id)}
+          />{" "}
+          {proizvod.nazivArtikla} - {proizvod.cenaArtikla} RSD -{" "}
+          {proizvod.kategorija}
+          <button onClick={() => obrisiProizvod(proizvod.id)}>Obriši</button>
+        </p>
       ))}
+      <p>Ukupno proizvoda u korpi: {prikazaniArtikli.length}</p>
+      <p>Ukupna cena kupljenih proizvoda je : {ukupnaCenaKupljenih}</p>
     </div>
   );
 }
